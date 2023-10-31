9815beb43857a9ba315d7fd2ae9e420c98ad31d2
Marc Sogas Esteve <marc.sogas@gmail.com>
2023-10-31 15:02:04 +0100

    Add navigation component library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    val navVersion = "2.7.1"
+    
+    //NavComponent
+    implementation ("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation ("androidx.navigation:navigation-ui-ktx:$navVersion")
 }
