70a4e407e4073051ad0ec17f0383ec2976cc9985
Marc Sogas Esteve <marc.sogas@gmail.com>
2023-10-31 14:58:00 +0100

    add fragments

---
+++ app/src/main/java/com/marcsedev/horoscoapp/ui/luck/LuckFragment.kt
@@ -0,0 +1,24 @@
+package com.marcsedev.horoscoapp.ui.luck
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.marcsedev.horoscoapp.R
+import com.marcsedev.horoscoapp.databinding.FragmentHoroscopeBinding
+import com.marcsedev.horoscoapp.databinding.FragmentLuckBinding
+
+class LuckFragment : Fragment() {
+
+    private var _binding: FragmentLuckBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
