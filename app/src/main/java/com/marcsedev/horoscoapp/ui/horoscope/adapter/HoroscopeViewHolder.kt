862782285bdf226ff81498bdbd32c4b52ca5cb06
Marc Sogas Esteve <marc.sogas@gmail.com>
2023-11-02 12:29:30 +0100

    add animation

--- app/src/main/java/com/marcsedev/horoscoapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
+++ app/src/main/java/com/marcsedev/horoscoapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
@@ -1,6 +1,7 @@
 package com.marcsedev.horoscoapp.ui.horoscope.adapter
 
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.recyclerview.widget.RecyclerView
 import com.marcsedev.horoscoapp.databinding.ItemHoroscopeBinding
 import com.marcsedev.horoscoapp.domain.moel.HoroscopeInfo
@@ -9,11 +10,25 @@ class HoroscopeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val binding = ItemHoroscopeBinding.bind(view)
 
-    fun render(horoscopeInfo: HoroscopeInfo, onItemSelected:(HoroscopeInfo) -> Unit) {
+    fun render(horoscopeInfo: HoroscopeInfo, onItemSelected: (HoroscopeInfo) -> Unit) {
         val context = binding.tvTitle.context
         binding.ivHoroscope.setImageResource(horoscopeInfo.img)
         binding.tvTitle.text = context.getString(horoscopeInfo.name)
 
-        binding.parent.setOnClickListener { onItemSelected(horoscopeInfo) }
+        binding.parent.setOnClickListener {
+            startRotationAnimation(
+                binding.ivHoroscope,
+                newLambda = { onItemSelected(horoscopeInfo) })
+        }
+    }
+
+    fun startRotationAnimation(view: View, newLambda: () -> Unit) {
+        view.animate().apply {
+            duration = 500
+            interpolator = LinearInterpolator()
+            rotationBy(360f)
+            withEndAction { newLambda() }
+            start()
+        }
     }
 }
